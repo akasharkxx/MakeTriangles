4f01e1274b9d7a92937f459470aea24014c3ab7c
R1pHunt3r <akash.jaiswar41@gmail.com>
2022-11-22 20:19:51 +0530

    Added material editor using ImGUI

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,18 +1,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "stb_image.h"
-#include "shader.h"
-#include "camera.h"
 #include <iostream>
+
+#include "includes/stb_image.h"
+#include "includes/shader.h"
+#include "includes/camera.h"
+#include "includes/imgui/imgui.h"
+#include "includes/imgui/imgui_impl_glfw.h"
 
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
+#include "includes/imgui/imgui_impl_opengl3.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void mousescroll_callback(GLFWwindow* window, double xOffset, double yOffset);
+void mousebutton_callback(GLFWwindow* window, int button, int action, int mods);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -30,8 +35,11 @@ float fov = 45.0f;
 float mixValue = 0.0f;
 
 bool firstMouse = true;
+bool enableMouse = false;
 
 glm::vec3 lightPos(1.2, 1.0f, 2.0f);
+
+const char* glsl_version = "#version 130";
 
 int main()
 {
@@ -52,9 +60,10 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, mousescroll_callback);
+    glfwSetMouseButtonCallback(window, mousebutton_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -65,8 +74,8 @@ int main()
     glEnable(GL_DEPTH_TEST);
 
     // Loading shaders
-    Shader lightShader("light_color.vs", "light_color.fsc");
-    Shader lightCubeShader("light_object.vs", "light_object.fsc");
+    Shader lightShader("src/shaders/light_color.vs", "src/shaders/light_color.fsc");
+    Shader lightCubeShader("src/shaders/light_object.vs", "src/shaders/light_object.fsc");
 
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -138,8 +147,28 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    ImGui::StyleColorsDark();
+
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImVec4 ambient_light_color = ImVec4(0.2f, 0.2f, 0.2f, 1.0f);
+    ImVec4 diffuse_light_color = ImVec4(0.5f, 0.5f, 0.5f, 1.0f);
+    ImVec4 specular_light_color = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+    ImVec4 material_ambient_color = ImVec4(1.0f, 0.5f, 0.31f, 1.0f);
+    ImVec4 material_diffuse_color = ImVec4(1.0f, 0.5f, 0.31f, 1.0f);
+    ImVec4 material_specular_color = ImVec4(0.5f, 0.5f, 0.05f, 1.0f);
+
     while (!glfwWindowShouldClose(window))
     {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
         float currentTime = static_cast<float>(glfwGetTime());
         deltaTime = currentTime - lastFrame;
         lastFrame = currentTime;
@@ -151,12 +180,41 @@ int main()
 
         processInput(window);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        static float shininess = 32.0f;
+
+        ImGui::Begin("Material Editor");                          // Create a window called "Hello, world!" and append into it.
+
+        //ImGui::SliderFloat3("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+        ImGui::Text("Light color");
+        ImGui::ColorEdit3("Light Ambient Color", (float*)&ambient_light_color);
+        ImGui::ColorEdit3("Light Diffuse Color", (float*)&diffuse_light_color);
+        ImGui::ColorEdit3("Light Specular Color", (float*)&specular_light_color);
+
+        ImGui::Text("Material color");
+        ImGui::ColorEdit3("Material Ambient Color", (float*)&material_ambient_color);
+        ImGui::ColorEdit3("Material Diffuse Color", (float*)&material_diffuse_color);
+        ImGui::ColorEdit3("Material Specular Color", (float*)&material_specular_color);
+
+        ImGui::DragFloat("Shininess", &shininess, 2.0f, 256.0f);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+
         lightShader.use();
-        lightShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-        lightShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+        lightShader.setVec3("objectColor", 0.5f, 0.4f, 0.3f);
+        lightShader.setVec3("lightColor", 1.0f, 0.0f, 1.0f);
+
+        lightShader.setVec3("material.ambient", material_ambient_color.x, material_ambient_color.y, material_ambient_color.z);
+        lightShader.setVec3("material.diffuse", material_diffuse_color.x, material_diffuse_color.y, material_diffuse_color.z);
+        lightShader.setVec3("material.specular", material_specular_color.x, material_specular_color.y, material_specular_color.z);
+        lightShader.setFloat("material.shininess", shininess);
+        lightShader.setVec3("light.ambient", ambient_light_color.x, ambient_light_color.y, ambient_light_color.z);
+        lightShader.setVec3("light.diffuse", diffuse_light_color.x, diffuse_light_color.y, diffuse_light_color.z);
+        lightShader.setVec3("light.specular", specular_light_color.x, specular_light_color.y, specular_light_color.z);
 
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
@@ -164,10 +222,6 @@ int main()
         lightShader.setMat4("view", view);
         lightShader.setVec3("lightPos", lightPos);
         lightShader.setVec3("viewPos", camera.Position);
-        lightShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-        lightShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-        lightShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-        lightShader.setFloat("material.shininess", 32.0f);
 
         // world transformation
         glm::mat4 model = glm::mat4(1.0f);
@@ -180,6 +234,7 @@ int main()
 
         // Lamp object
         lightCubeShader.use();
+        lightCubeShader.setVec4("lightColor", ambient_light_color.x, ambient_light_color.y, ambient_light_color.z, 1.0f);
         lightCubeShader.setMat4("projection", projection);
         lightCubeShader.setMat4("view", view);
         model = glm::mat4(1.0f);
@@ -190,6 +245,9 @@ int main()
         glBindVertexArray(lightCubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -198,8 +256,29 @@ int main()
     glDeleteVertexArrays(1, &lightCubeVAO);
     glDeleteBuffers(1, &VBO);
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     glfwTerminate();
     return 0;
+}
+
+void mousebutton_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        enableMouse = !enableMouse;
+        if (enableMouse)
+        {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        }
+        else
+        {
+            firstMouse = true;
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }
+    }
 }
 
 void mousescroll_callback(GLFWwindow* window, double xOffset, double yOffset)
@@ -209,6 +288,8 @@ void mousescroll_callback(GLFWwindow* window, double xOffset, double yOffset)
 
 void mouse_callback(GLFWwindow* window, double xPos, double yPos)
 {
+    if (!enableMouse) return;
+
     if (firstMouse) 
     {
         lastMouseX = xPos;
